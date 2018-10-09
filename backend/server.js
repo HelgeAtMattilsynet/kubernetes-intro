29dfc71862fcea44a378f7bd0639a4f69ef7a44c
HelgeAtMattilsynet <helge.langseth@mattilsynet.no>
2018-10-09 10:03:41 +0200

    Personalized it

--- backend/server.js
+++ backend/server.js
@@ -9,17 +9,17 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/me', (request, response) => {
-	response.send('{"name": "Arthur Dent", "location": "Earth"}');
+	response.send('{"name": "Helge Langseth", "location": "Earth"}');
 	logger.info('Responded to GET request on /api/me')
 });
 
 app.get('/api/work', (request, response) => {
-	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
+	response.send('[{"yearFrom": "2011", "yearTo": "2018", "place": "Mattilsynet", "comment": "Am doing stuff"}, {"yearFrom": "2004", "yearTo": "2011", "place": "Diakonhjemmet Sykehus", "comment": "Was doing stuff"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
 app.get('/api/education', (request, response) => {
-	response.send('[{"yearFrom": "2005", "yearTo": "2007", "place": "University 1", "comment": "Studying"}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]');
+	response.send('[{"yearFrom": "1996", "yearTo": "1999", "place": "Gjøvik College", "comment": "Studied"}, {"yearFrom": "1999", "yearTo": "2002", "place": "Oslo University", "comment": "Studied some more"}]');
 	logger.info('Responded to GET request on /api/edu')
 });
 
